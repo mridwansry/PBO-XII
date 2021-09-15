e3b08b66bb72fdb603aae3d74bd3b36053269a37
mridwansry <77963070+mridwansry@users.noreply.github.com>
2021-09-15 20:54:14 +0700

    Revisi PERTEMUAN 7
    Ada Kesalahan data dalam menghapus data yang di inginkan

--- tugasmvc-pertemuan7/Views/View_siswa.php
+++ tugasmvc-pertemuan7/Views/View_siswa.php
@@ -45,7 +45,7 @@ $GetSiswa = $siswa->GetData_All();
                 <td><?php echo $Get['nominal']; ?></td>
                 <td>
                     <a href="../Views/View_put_siswa.php?nisn=<?php echo base64_encode($Get['nisn']) ?>"><img src="view.png" width="17"></a>
-                    <button onclick=" konfirmasi(<?php echo $Get['nisn'] ?>)">Delete</button>
+                    <button onclick="konfirmasi('<?php echo base64_encode($Get['nisn']) ?>')">Delete</button>
             </tr>
     <?php
         }
@@ -55,8 +55,9 @@ $GetSiswa = $siswa->GetData_All();
 
 <script>
     function konfirmasi(nisn) {
+        var a = nisn;
         if (window.confirm('apakah anda ingin menghapus data ini ?')) {
-            window.location.href = '../Config/Routes.php?function=delete_siswa&nisn=<?php echo base64_encode($Get['nisn']) ?>';
+            window.location.href = '../Config/Routes.php?function=delete_siswa&nisn=' + a;
         };
     }
 </script>
